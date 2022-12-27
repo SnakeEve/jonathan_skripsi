f5dedb6d7a431a52104e374f3a00f783e5052f18
Marcel <marcellino@mobilecom.co.id>
2022-12-28 01:15:53 +0700

    rekomendasi

---
+++ history_rekomendasi.php
@@ -0,0 +1,60 @@
+<style type="text/css">
+.callout {
+    padding: 20px;
+    margin: 20px 0;
+    border: 1px solid #eee;
+    border-left-width: 5px;
+    border-radius: 3px;
+    h4 {
+        margin-top: 0;
+        margin-bottom: 5px;
+    }
+    p:last-child {
+        margin-bottom: 0;
+    }
+    code {
+        border-radius: 3px;
+    }
+    & + .bs-callout {
+        margin-top: -5px;
+    }
+}
+</style>
+
+<main role="main">
+    <div class="container pt-4">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <a class="btn btn-md btn-primary" href="index.php?page=rekomendasi_jurusan">Lakukan tes ulang</a>
+                <br>
+                <br>
+                <h6 class="text-center">Daftar jurusan kuliah di bawah ini telah diurutkan berdasarkan jawaban kamu pada kuis minat bakat</h6>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="callout">
+                    <h4>Teknik Fisika</h4>
+                    4 Kampus<br>
+                    Rp 500.000 - Rp 25.000.000/Semester
+                </div>
+                <div class="callout">
+                    <h4>Teknik Nuklir</h4>
+                    1 Kampus<br>
+                    Rp 500.000 - Rp 13.000.000/Semester
+                </div>
+                <div class="callout">
+                    <h4>Ilmu Komputer</h4>
+                    11 Kampus<br>
+                    Rp 500.000 - Rp 11.000.000/Semester
+                </div>
+                <div class="callout">
+                    <h4>Sistem Informasi</h4>
+                    14 Kampus<br>
+                    Rp 500.000 - Rp 25.000.000/Semester
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
---
+++ rekomendasi_jurusan.php
@@ -0,0 +1,27 @@
+<main role="main">
+    <div class="container pt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-center">
+                    <h3>Cari jurusan kuliah yang cocok untuk kamu</h3>
+                    <h5 id="rekomendasi_question">Silahkan Jawab 25 Pertanyaan umum di bawah ini untuk menentukan jurusan yang paling cocok untuk anda!</h5>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="rekomendasi_answer" id="question_counter">1/25</label>
+                    <textarea class="form-control" id="rekomendasi_answer" rows="6"></textarea>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-danger form-control btn-block">No</button>
+            </div>
+            <div class="col">
+                <button id="btnSubmit" class="btn btn-primary form-control btn-block" type="submit">Yes</button>
+            </div>
+        </div>
+
+    </div>
+</div>
