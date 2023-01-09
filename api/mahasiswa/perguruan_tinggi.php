aeff8df51afd18226f62bcb30f9e643f1cd385bd
Lucky <ovr-lucky.satriawan@ajaib.co.id>
2023-01-09 22:52:07 +0700

    api perguruan tinggi search bugfix

--- api/mahasiswa/perguruan_tinggi.php
+++ api/mahasiswa/perguruan_tinggi.php
@@ -13,7 +13,7 @@ if(isset($_GET['apiname'])){
             $data = json_decode($body, true);
 
             $sqlSearchNama = "";
-            if(sizeof($data) > 0 && $data["nama"] != null){
+            if(sizeof($data) > 0 && isset($data['nama'])){
                 $nama = $data['nama'];
                 $sqlSearchNama  = $sqlSearchNama . "AND upper(nama) like upper('%$nama%') ";
             }
